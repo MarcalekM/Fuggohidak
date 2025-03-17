04c8a6e368af376c8989e2108d533db38ca972b9
Sufi <120452154+MarcalekM@users.noreply.github.com>
2025-03-17 10:25:52 +0100

    Add project files.

---
+++ Fuggohid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuggohidak
+{
+    class Fuggohid
+    {
+        public string Helyezes { get; set; }
+        public string Nev { get; set; }
+        public string Hely { get; set; }
+        public int Hossz { get; set; }
+        public int Ev { get; set; }
+
+        public Fuggohid(string sor)
+        {
+            var temp = sor.Split('\t');
+            Helyezes = temp[0];
+            Nev = temp[1];
+            Hely = temp[2];
+            Hossz = int.Parse(temp[3]);
+            Ev = int.Parse(temp[4]);
+        }
+    }
+}
